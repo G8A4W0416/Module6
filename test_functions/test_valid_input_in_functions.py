a69c2d8c8a8813cf34d1f81d2fb6e3bc95232ac1
G8A4W0416 <wilhelmg1@gmail.com>
2020-02-26 22:14:09 -0600

    Third commit of Function Default Values Assignment. score_input() function is written so test_score_input_test_name and test_score_input_test_score_valid passes. Corrected expected values in the two tests.

--- test_functions/test_valid_input_in_functions.py
+++ test_functions/test_valid_input_in_functions.py
@@ -4,10 +4,10 @@ from more_functions import validate_input_in_functions
 
 class MyTestCase(unittest.TestCase):
     def test_score_input_test_name(self):
-        self.assertEqual("Greg", validate_input_in_functions.score_input("Greg"))
+        self.assertEqual("Greg: 0", validate_input_in_functions.score_input("Greg"))
 
     def test_score_input_test_score_valid(self):
-        self.assertEqual(100, validate_input_in_functions.score_input("Greg", 100))
+        self.assertEqual("Greg: 100", validate_input_in_functions.score_input("Greg", 100))
 
     def test_score_input_test_score_below_range(self):
         self.assertEqual("Invalid test score, try again!", validate_input_in_functions.score_input("Greg", -1))
