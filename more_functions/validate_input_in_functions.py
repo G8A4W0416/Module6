2a53c7ff04f46efc701835272402b627bef51b39
G8A4W0416 <wilhelmg1@gmail.com>
2020-02-26 21:51:08 -0600

    Initial commit of Function Default Values Assignment. This has all unit tests and score_input() function returning pass. All unit tests do not pass at this point.

---
+++ more_functions/validate_input_in_functions.py
@@ -0,0 +1,11 @@
+def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again!'):
+    """ This takes in a test name, test score, and invalid message. The user is prompted for a valid test score until it
+    is in the range of 0-100, then prints out the valid input as 'Test name: ##'.
+    :param test_name: String name passed in
+    :param test_score: Int score passed in. Optional, with default value of 0 (zero)
+    :param invalid_message: String invalid message passed in. Optional, with default value of
+    'Invalid test score, try again!'
+    :return: String showing test name with valid test score
+    """
+    # return { test_name: test_score}
+    return pass
---
+++ test_functions/test_valid_input_in_functions.py
@@ -0,0 +1,27 @@
+import unittest
+from more_functions import validate_input_in_functions
+
+
+class MyTestCase(unittest.TestCase):
+    def test_score_input_test_name(self):
+        self.assertEqual("Greg", validate_input_in_functions.score_input("Greg"))
+
+    def test_score_input_test_score_valid(self):
+        self.assertEqual("Greg", validate_input_in_functions.score_input("Greg", 100))
+
+    def test_score_input_test_score_below_range(self):
+        self.assertEqual("Greg", validate_input_in_functions.score_input("Greg", -1))
+
+    def test_score_input_test_score_above_range(self):
+        self.assertEqual("Greg", validate_input_in_functions.score_input("Greg", 101))
+
+    def test_score_input_test_score_non_numeric(self):
+        self.assertEqual("Greg", validate_input_in_functions.score_input("Greg", "Wilhelm"))
+
+    def test_score_input_invalid_message(self):
+        self.assertEqual("Greg", validate_input_in_functions.score_input("Greg", 100,
+                                                                         "Please enter a value between 0-100"))
+
+
+if __name__ == '__main__':
+    unittest.main()
