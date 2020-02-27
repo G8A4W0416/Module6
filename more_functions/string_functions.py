5d8871047b9fab8b371baa51f6380beb5606d5e0
G8A4W0416 <wilhelmg1@gmail.com>
2020-02-26 20:42:21 -0600

    Completed multiply_string function to get test_multiple_string to pass.

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -1,2 +1,8 @@
 def multiply_string(message, n):
-    pass
+    """ This repeats the message passed in n number of times
+        :param message: String message passed in
+        :param n: Int number of times to repeat message
+        :return: String showing message repeated n number of times
+        """
+    msg_repeated = message * n
+    return msg_repeated
